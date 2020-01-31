5d4ffae32afdda0d8ac68f0208420277064d050b
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-01-30 22:55:14 -0300

    Finished lesson 12 of chapter 3

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+   dialect: 'mysql',
+   host: 'localhost',
+   username: 'root',
+   password: '',
+   database: 'go_barber',
+   define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true
+   }
+}
