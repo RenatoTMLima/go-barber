13cfcb44990cbf56d4cd2852399d23226dabd43d
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-02-03 22:25:14 -0300

    Finished lesson 3 of chapter 4

---
+++ backend/src/app/controllers/ProviderController.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+
+class ProviderController{
+   async index(req, res){
+      const providers = await User.findAll({
+         where: {provider: true},
+         attributes: [
+            'id',
+            'name',
+            'email',
+            'avatar_id'
+         ],
+         include: ['avatar']
+      })
+
+      return res.json(providers);
+   }
+}
+
+export default new ProviderController;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,28 @@
 import {Router} from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
+const upload = multer(multerConfig);
 
+routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(auth);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
