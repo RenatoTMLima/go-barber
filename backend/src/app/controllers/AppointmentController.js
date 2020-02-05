c7c869f605d1967c89c079c93f85c037d89fb29a
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-02-05 16:06:42 -0300

    Finished lesson 18 from chapter 4

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -108,6 +108,10 @@ class AppointmentController {
             model: User,
             as: 'provider',
             attributes: ['name', 'email']
+         },{
+            model: User,
+            as: 'user',
+            attributes: ['name']
          }]
       });
 
@@ -128,7 +132,12 @@ class AppointmentController {
       await Mail.sendMail({
          to: `${appointment.provider.name} <${appointment.provider.email}>`,
          subject: 'Agendamento cancelado',
-         text: 'Você tem um novo cancelamento'
+         template: 'cancellation',
+         context: {
+            provider: appointment.provider.name,
+            user: appointment.user.name,
+            data: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm", {locale: pt})
+         }
       });
 
       return res.json(appointment);
---
+++ backend/src/config/redis.js
@@ -0,0 +1,5 @@
+export default {
+   host: '127.0.0.1',
+   port: '17874',
+   url: 'redis-17874.c17.us-east-1-4.ec2.cloud.redislabs.com'
+}
