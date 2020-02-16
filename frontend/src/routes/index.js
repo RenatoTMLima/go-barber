ed08a349c5372289c25a32154b8e34b268652052
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-02-16 20:38:05 -0300

    Finished lesson 9 from chapter 10

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {Switch} from 'react-router-dom';
 import Route from './Route';
 
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
-import SignUp from '../pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
+import SignUp from '~/pages/SignUp';
+import SignIn from '~/pages/SignIn';
 
 export default function Routes(){
    return (
