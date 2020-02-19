b2580666a81352cd5a896d17e675ea2451e4e6a2
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-02-19 14:37:02 -0300

    Finished lesson 21 from chapter 10

--- frontend/src/pages/_layouts/default/index.js
+++ frontend/src/pages/_layouts/default/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import propTypes from  'prop-types';
 
+import Header from '~/components/Header';
+
 import { Wrapper } from './styles';
 
 export default function DefaultLayout({children}) {
   return (
-    <Wrapper>{children}</Wrapper>
+    <Wrapper>
+      <Header />
+      {children}
+    </Wrapper>
   );
 }
 
