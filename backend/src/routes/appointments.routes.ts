053971e7f4cac119c96e2e0a455001a1b5951383
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-04-15 22:53:12 -0300

    Stopped at the video Repositorio do TypeORM at 12:40

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,30 +1,29 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 
+import { getCustomRepository } from 'typeorm';
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-const appointmentsRepository = new AppointmentsRepository();
-
 appointmentsRouter.get('/', (request, response) => {
-   const appointments = appointmentsRepository.all();
+   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+   const appointments = appointmentsRepository.find();
 
    return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
    try {
       const { provider, date } = request.body;
 
       const parsedDate = parseISO(date);
 
-      const createAppointmentService = new CreateAppointmentService(
-         appointmentsRepository,
-      );
+      const createAppointmentService = new CreateAppointmentService();
 
-      const appointment = createAppointmentService.execute({
+      const appointment = await createAppointmentService.execute({
          provider,
          date: parsedDate,
       });
