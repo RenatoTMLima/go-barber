68d20f4fe025305be05ea51a219f644c3b0473e3
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-04-23 22:58:48 -0300

    Finished styling the login component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyles />
+  </>
+);
+
+export default App;
