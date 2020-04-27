1e95210e235423f80a97a2746366980e0ba6960d
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-04-26 23:13:34 -0300

    Finished validation lesson

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import {Form} from '@unform/web';
-
+import React, { useCallback, useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
 
@@ -11,16 +13,36 @@ import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: Object): void{
+  const formRef = useRef<FormHandles>(null);
 
-  }
-  
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
@@ -33,7 +55,6 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-
         </Form>
 
         <a href="lhbh">
@@ -41,9 +62,8 @@ const SignUp: React.FC = () => {
           Voltar para logon
         </a>
       </Content>
-      
     </Container>
-  )
-}
+  );
+};
 
 export default SignUp;
