6bd5e018a3433c925e5cfce4975342ada0e5e64b
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-04-30 22:30:17 -0300

    Redone some things from the lesson

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,10 +1,13 @@
+// import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 const App: React.FC = () => (
-  <View>
-    <Text>App</Text>
-  </View>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
 );
 
 export default App;
