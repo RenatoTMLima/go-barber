76121a88a32754e9049c0f82c48ea0c702e21c65
Renato Lima <renato.lima23@fatec.sp.gov.br>
2020-06-06 08:32:36 -0300

    Added the providers route and logic

--- backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,6 +1,7 @@
 import { uuid } from 'uuidv4';
 
 import ICreateUserDTO from '@modules/users/dtos/iCreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 import IUsersRepository from '@modules/users/repositories/iUsersRepository';
 
 import User from '../../infra/typeorm/entities/User';
@@ -18,6 +19,16 @@ class FakeUsersRepository implements IUsersRepository {
       const findUser = this.users.find(user => user.email === email);
 
       return findUser;
+    }
+
+    public async findAllProviders({except_user_id}: IFindAllProvidersDTO): Promise<User[]> {
+      let users = this.users;
+
+      if(except_user_id){
+        users = this.users.filter(user => user.id !== except_user_id);
+      }
+
+      return users;
     }
 
     public async create(userData: ICreateUserDTO): Promise<User>{
--- backend/src/modules/users/repositories/iUsersRepository.ts
+++ backend/src/modules/users/repositories/iUsersRepository.ts
@@ -1,9 +1,11 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/iCreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 export default interface IUsersRepository {
-    findByEmail(email: string): Promise<User | undefined>,
-    findById(id: string): Promise<User | undefined>,
+    findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
+    findByEmail(email: string): Promise<User | undefined>;
+    findById(id: string): Promise<User | undefined>;
     create(data: ICreateUserDTO): Promise<User>;
     save(user: User): Promise<User>;
 }
